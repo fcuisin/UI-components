912b4bd6e65b01548cf5585b69b0d85c59a71daa
Ferdinand Cuisin <fcuisin@gmail.com>
2021-03-03 16:02:15 +0100

    Create a display function to show all plates

--- add-to-cart/script.js
+++ add-to-cart/script.js
@@ -2,6 +2,18 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
 
+function renderPlates(items = [], itemsList) {
+
+  itemsList.innerHTML = items.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.checked ? 'checked' : ''} />
+        <label for="item${i}">${plate.name}</label>
+      </li>
+    `;
+  }).join('');
+}
+
 function createPlate(e) {
   e.preventDefault();
   const text = this.querySelector('[name=item]').value;
@@ -10,6 +22,7 @@ function createPlate(e) {
     checked: false
   }
   items.push(item);
+  renderPlates(items, itemsList);
   this.reset();
 }
 
